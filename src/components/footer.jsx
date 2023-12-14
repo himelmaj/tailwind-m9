6e69fcfec345cf89b7b8e97d2a0184dd6fd573b8
Himel Majumder <himelmajumder1@gmail.com>
2023-12-15 00:19:46 +0100

    Update favicon

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="mx-auto mt-10 py-10 text-center"> &copy; 2023 Ñude createad by Himel Majumder</footer>
+    <footer className="mx-auto mt-10 py-10 text-center"> &copy; 2023 Ñude created by Himel Majumder</footer>
   )
 }
 
